7c4a46e70f1a554903c611c15e3fe7c3d51ced95
xayanide <xayanide@gmail.com>
2025-03-05 10:08:19 +0800

    chore: include jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** From https://github.com/kulshekhar/ts-jest/blob/main/examples/js-with-ts/jest-esm.config.mjs */
+import { createJsWithTsEsmPreset } from "ts-jest";
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+    displayName: "js-with-ts",
+    testMatch: ["**/test/**/*.test.ts"],
+    ...createJsWithTsEsmPreset({
+        tsconfig: "./tsconfig.json",
+    }),
+    coverageDirectory: "./coverage",
+    coveragePathIgnorePatterns: ["node_modules"],
+};
