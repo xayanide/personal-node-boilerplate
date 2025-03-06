560f809472f66ddcd6e5758940a170c3257b1aa7
xayanide <xayanide@gmail.com>
2025-03-06 21:23:54 +0800

    chore: update tests

--- src/__test__/testenv.test.ts
+++ src/__test__/testenv.test.ts
@@ -1,7 +1,9 @@
 describe("Given a TypeScript ESM test environment", function () {
-    describe("When encountering global variables jest and import.meta", function () {
-        test("Then import.meta.jest and import.meta.url are available", function () {
+    describe("When using global variables jest and import.meta", function () {
+        test("Then 'jest' as a global variable is undefined", function () {
             expect(typeof jest === "undefined").toBeTruthy();
+        });
+        test("Then import.meta.jest and import.meta.url are available", function () {
             const importMeta = import.meta;
             expect(typeof importMeta.jest === "object").toBeTruthy();
             expect(typeof importMeta.url === "string").toBeTruthy();
