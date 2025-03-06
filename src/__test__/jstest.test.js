3f3f9c945a3eb12e1b31384bc60a5b1a721598b2
xayanide <xayanide@gmail.com>
2025-03-06 18:35:46 +0800

    chore: refine test

--- src/__test__/jstest.test.js
+++ src/__test__/jstest.test.js
@@ -1,9 +1,10 @@
 describe("Given a JavaScript ESM test environment", function () {
     describe("When encountering global variables jest and import.meta", function () {
-        test("Then jest and import.meta properties are available", function () {
+        test("Then import.meta.jest and import.meta.url are available", function () {
             expect(typeof jest === "undefined").toBeTruthy();
-            expect(typeof import.meta.jest === "object").toBeTruthy();
-            expect(typeof import.meta.url === "string").toBeTruthy();
+            const importMeta = import.meta;
+            expect(typeof importMeta.jest === "object").toBeTruthy();
+            expect(typeof importMeta.url === "string").toBeTruthy();
         });
     });
 });
