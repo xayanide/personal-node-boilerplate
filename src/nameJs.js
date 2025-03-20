5df8b032d053eac138d955b654f96b73f8852bc0
xayanide <xayanide@gmail.com>
2025-03-21 02:06:53 +0800

    chore: ok

--- src/nameJs.js
+++ src/nameJs.js
@@ -3,8 +3,11 @@ export function greet(personName) {
         throw new Error("Invalid name provided. Must be a string.");
     }
     const trimmedPersonName = personName.trim();
-    if (trimmedPersonName === "") {
+    if (trimmedPersonName.replace(/\s+/gu, "").length === 0) {
         throw new Error("Invalid name provided. Must be a non-empty string.");
+    }
+    if (trimmedPersonName.length > 1000) {
+        throw new Error("Invalid name provided. Name is too long.");
     }
     return `Hello, ${trimmedPersonName}!`;
 }
