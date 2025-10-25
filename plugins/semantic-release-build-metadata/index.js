7ee2b086d1aab89ed4fc6a15529af557dd37b899
xayanide <xayanide@gmail.com>
2025-10-25 22:03:05 +0800

    fix: again

--- plugins/semantic-release-build-metadata/index.js
+++ plugins/semantic-release-build-metadata/index.js
@@ -1,22 +1,13 @@
 import { prepare as npmPrepare } from "@semantic-release/npm";
-import fs from "fs";
 import { execSync } from "child_process";
 
 export function prepare(pluginConfig, context) {
-    const { nextRelease, logger } = context;
+    const { nextRelease } = context;
 
     const timestamp = new Date().toISOString().replace(/[-:.]/g, "");
     const gitHash = execSync("git rev-parse --short HEAD").toString().trim();
 
     const customVersion = `${nextRelease.version}+${timestamp}.githash.${gitHash}`;
-
-    logger.log(`Overriding version: ${nextRelease.version} → ${customVersion}`);
-
-    const pkgPath = "package.json";
-    const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf-8"));
-    pkg.version = customVersion;
-    fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + "\n");
-
     return npmPrepare(pluginConfig, {
         ...context,
         nextRelease: { ...nextRelease, version: customVersion },
