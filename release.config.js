7240045927beaa07e8af0e1c48ddfa4984415843
xayanide <xayanide@gmail.com>
2025-10-25 22:58:45 +0800

    fix: ugh

--- release.config.js
+++ release.config.js
@@ -1,25 +1,20 @@
 import { execSync } from "node:child_process";
+
+function a() {
+    const timestamp = new Date().toISOString().replace(/[-:.TZ]/g, "");
+    const gitHash = execSync("git rev-parse --short HEAD").toString().trim();
+    return `+${timestamp}.githash.${gitHash}`;
+}
+
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-    tagFormat: "${version}",
+    tagFormat: "${version}" + a(),
     branches: ["main", "dev"],
     plugins: [
         ["@semantic-release/commit-analyzer"],
         ["@semantic-release/release-notes-generator"],
-        [
-            {
-                prepare: (_, context) => {
-                    const { nextRelease } = context;
-                    const timestamp = new Date().toISOString().replace(/[-:.TZ]/g, "");
-                    const gitHash = execSync("git rev-parse --short HEAD").toString().trim();
-                    const customVersion = `${nextRelease.version}+${timestamp}.githash.${gitHash}`;
-                    context.nextRelease.version = customVersion;
-                    context.logger.log(`Using version: ${nextRelease.version}`);
-                },
-            },
-        ],
         // { npmPublish } is disabled by default only if the "private" property
         // in package.json is true.
         ["@semantic-release/npm", { npmPublish: false }],
