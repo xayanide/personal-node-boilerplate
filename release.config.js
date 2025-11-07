f339f1336048e1df65386fa5fb927b693b490e0a
xayanide <xayanide@gmail.com>
2025-11-08 01:12:43 +0800

    chore: update releaser

--- release.config.js
+++ release.config.js
@@ -3,7 +3,7 @@
  */
 export default {
     tagFormat: "${version}",
-    branches: ["main", "dev"],
+    branches: ["main", { name: "dev", prerelease: true }],
     plugins: [
         ["@semantic-release/commit-analyzer"],
         ["@semantic-release/release-notes-generator"],
